b7b482a02ec2b21671bb8bbffaad1c57b6287c88
SnazzyFox <12706268+snazzyfox@users.noreply.github.com>
2022-07-27 01:02:34 -0700

    Update wording

--- src/components/Config/Player.svelte
+++ src/components/Config/Player.svelte
@@ -1,8 +1,9 @@
 <h2>Widget Settings</h2>
 <div class="form-item mbe8">
-    <Input label="Auto-hide player after seconds" bind:value={$autoHideTime} type="number" placeholder="Always Visible"/>
-    Automatically hide the player after this many seconds. Leave this box blank if the player should be
-    displayed continuously when music is playing.
+    <Input label="Auto-hide player after (seconds)" bind:value={$autoHideTime} type="number" min=1 placeholder="Always Visible"/>
+    Automatically hide the player after this many seconds passes. The player will reappear automatically when a new track starts playing, 
+    or when someone invokes it using the "show player command" (if Twitch integration is enabled). If you want the player to be on screen
+    at all times, leave this box blank.
 </div>
 
 <script lang="ts">
